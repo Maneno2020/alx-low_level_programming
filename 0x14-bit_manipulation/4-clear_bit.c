c8f79a9b700358badab1e550c5f0e91da8ccff7f
Maneno2020 <2020akcs0098gf@kab.ac.ug>
2023-05-04 11:35:37 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,18 +2,18 @@
 
 /**
  * clear_bit - initialize the value of a bit to 0.
- * at a given index.
- * @n: reference of an unsigned long int.
+ * at a particular index.
+ * @n: reference to an unsigned long int.
  * @index: position of the bit.
  *
- * Return: 1 if it worked, 0 if it didn't.
+ * Return: 1 if it worked, -1 if it didn't.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int m;
 
 	if (index > 63)
-		return (0);
+		return (-1);
 
 	m = 1 << index;
 
