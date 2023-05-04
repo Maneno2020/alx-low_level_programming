2ddd9be0e76509f854a37196a4276af947fc5fe9
Maneno2020 <2020akcs0098gf@kab.ac.ug>
2023-05-04 10:31:38 -0700

    updated

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,6 +11,7 @@
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
+int _atoi(const char *s);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 unsigned int binary_to_uint(const char *b);
