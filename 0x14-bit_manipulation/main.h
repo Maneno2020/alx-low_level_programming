caae25bb581176b49fe8ffc69df66425f65a870c
Maneno2020 <2020akcs0098gf@kab.ac.ug>
2023-05-04 11:08:39 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,14 +7,14 @@
  * Desc: This file contains the prototypes for all functions
  * written in this project directory.
  */
-
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int _atoi(const char *s);
+int _putchar(char c);
+int get_endianness(void);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
-int _putchar(char c);
-int _atoi(const char *s);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-unsigned int binary_to_uint(const char *b);
 int set_bit(unsigned long int *n, unsigned int index);
-int get_endianness(void);
+int clear_bit(unsigned long int *n, unsigned int index);
+
 #endif /* MAIN_H */
